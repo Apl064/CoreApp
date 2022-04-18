3f444fbfa5f82dfa333705c0d2e7a302749baf64
apl064 <apl064@gmail.com>
2022-04-18 23:28:41 +0300

    Initial Commit

---
+++ CoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreApp
+//
+//  Created by Pavel Metelin on 18.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
